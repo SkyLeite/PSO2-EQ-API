02997c4633a0e2c6b93bb990b0e058a7858276e9
KazeSenoue <kazesenoue@gmail.com>
2016-10-27 17:48:45 -0200

    :D

--- Python/app.py
+++ Python/app.py
@@ -14,20 +14,14 @@ api = twitter.Api(consumer_key=consumer_key,
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+
+def translate(string):
+    with open('Python/eqs.json', encoding='utf8') as file:
+        file = json.load(file)
+        for key in file:
+            string = string.replace(key, file[key])
+
+        return string
 
 @app.errorhandler(404)
 def not_found(error):
@@ -35,51 +29,64 @@ def not_found(error):
 
 @app.route('/eq/api', methods=['GET'])
 def test():
-    output = {}
+    output = []
 
     shipEQ = re.compile(r'(\d*):([^0-9-―(\[]+)')
     preparation = re.compile(r'【開催中】\d*:\d*(.*)')
     jst = re.compile(r'＜(\d*)時 緊急クエスト予告＞')
-
-    def translate(string):
-        with open('eqs.json', encoding='utf8') as file:
-            file = json.load(file)
-            for key in file:
-                string = string.replace(key, file[key])
-
-            return string
 
     statuses = api.GetUserTimeline(screen_name='pso2_emg_hour')
 
     i = 0
     for status in statuses:
         i += 1
-        eq = {}
+        eqs = []
 
         for line in status.text.splitlines():
             if jst.match(line):
                 jsTime = jst.match(line).group(1)
 
             elif shipEQ.match(line):
-                eq.update({shipEQ.match(line).group(1) : translate(shipEQ.match(line).group(2).replace("　#PSO", ""))})
+                eqs.append({"ship" : shipEQ.match(line).group(1), "name" : translate(shipEQ.match(line).group(2).replace("　#PSO", ""))})
 
             elif preparation.match(line):
                 for x in range(1, 11):
-                    eq.update({"{:02d}".format(x) : translate(preparation.match(line).group(1).replace("　#PSO", ""))})
+                    eqs.append({"ship" : "{:02d}".format(x), "name" : translate(preparation.match(line).group(1).replace("　#PSO", ""))})
 
         jsTime = datetime.strptime(status.created_at, "%a %b %d %H:%M:%S %z %Y")
-        output.update({jsTime.strftime("%Y-%m-%d %H:%M:%S") : eq})
+        output.append({"time" : jsTime.strftime("%Y-%m-%d %H:%M:%S"), "eqs" : eqs})
 
 
-    return json.dumps(output, indent=4, sort_keys=True)
+    return json.dumps(output, indent=4)
 
+@app.route('/eq/api/ship/<int:ship_number>', methods=['GET'])
+def get_ship(ship_number):
+    output = []
+    statuses = api.GetUserTimeline(screen_name='pso2_emg_hour')
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+    shipEQ = re.compile(r'({:02d}):([^0-9-―(\[]+)'.format(ship_number))
+    preparation = re.compile(r'【開催中】\d*:\d*(.*)')
+    jst = re.compile(r'＜(\d*)時 緊急クエスト予告＞')
+
+    i = 0
+    for status in statuses:
+        i += 1
+        eqs = []
+
+        for line in status.text.splitlines():
+            if jst.match(line):
+                jsTime = jst.match(line).group(1)
+
+            elif shipEQ.match(line):
+                eqs.append({"ship" : shipEQ.match(line).group(1), "name" : translate(shipEQ.match(line).group(2).replace("　#PSO", ""))})
+
+            elif preparation.match(line):
+                eqs.append({"ship" : "{:02d}".format(ship_number), "name" : translate(preparation.match(line).group(1).replace("　#PSO", ""))})
+
+        jsTime = datetime.strptime(status.created_at, "%a %b %d %H:%M:%S %z %Y")
+        output.append({"time": jsTime.strftime("%Y-%m-%d %H:%M:%S"), "eqs": eqs})
+
+    return json.dumps(output, indent=4)
 
 if __name__ == '__main__':
     app.run(debug=True)
