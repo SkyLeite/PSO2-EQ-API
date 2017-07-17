efaaba80f9b6034145cf67aa4769d534bae874ac
UEM\rodrigo.fernandes <rodrigo.fernandes@uem.com.br>
2017-07-17 11:44:29 -0300

    wrong port

--- src/index.js
+++ src/index.js
@@ -80,9 +80,9 @@ app.get('/eq', async (req, res) => {
 });
 
 app.get('/eq/scheduled', async (req, res) => {
-    
+
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000.");
+app.listen(5000, () => {
+    console.log("Listening on port 5000.");
 });
