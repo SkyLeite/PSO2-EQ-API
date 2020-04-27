1064fbf423dfa1c8750d70aea2f3bf547050d285
Rodrigo Leite <rodrigo@leite.dev>
2020-04-27 15:30:04 -0300

    Use socket.io instead of ws

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,14 @@
-import * as WebSocket from "ws";
+import io from "socket.io";
 import Scraper from "./twitter";
 import config from "./config";
 
-const server = new WebSocket.Server({
-    port: config.port,
-});
+const server = io();
 
 const scraper = new Scraper();
 scraper.startPolling();
 
 scraper.eventEmitter.on("alert", response => {
-    server.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(response));
-        }
-    })
+    server.emit("quest", response);
 });
 
-const interval = setInterval(function ping() {
-    server.clients.forEach(client => {
-        if ((client as CustomWebsocket).isAlive === false) return client.terminate();
-
-        (client as CustomWebsocket).isAlive = false;
-        client.ping();
-    });
-}, 3000);
-
-server.on("connection", client => {
-    (client as CustomWebsocket).isAlive = true;
-
-    client.on('pong', function () {
-        (this as CustomWebsocket).isAlive = true;
-    });
-})
-
-server.on("close", () => {
-    clearInterval(interval);
-});
-
-interface CustomWebsocket extends WebSocket {
-    isAlive: boolean;
-}
+server.listen(8080)
